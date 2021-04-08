198446380adcfc96376da794c9afe00bff6ec3d0
mflem8 <matthewfleming8@gmail.com>
2021-04-08 19:46:55 -0400

    commit tree 61435a83f4ee288e4cc079a0b4074b8a39326f02

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+function map(array, callBack) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        let element = array[i]
+        newArray.push(callBack(element))
+    }
+    return newArray;
+}
+
+function reduce(array, callBack, startingPoint) {
+    let newArray = (!!startingPoint) ? startingPoint : array[0]
+    let i = (!!startingPoint) ? 0 : 1
+    for (; i < array.length; i++) {
+        newArray = callBack(array[i], newArray)
+    }
+    return newArray;
+}
